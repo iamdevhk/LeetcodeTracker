944cd16cbe9c94727594754b4cc6394dd49962de
HARIKRISHNAN RK <harikrishnan8888888@gmail.com>
2024-08-11 22:06:12 -0700

    added code for leetcode 53. Maximum Subarray

---
+++ maxSubarray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSoFar=nums[0]
+        maxEndingHere=nums[0]
+        for i in range(1,len(nums)):
+            maxEndingHere=max(maxEndingHere+nums[i],nums[i])
+            maxSoFar=max(maxEndingHere,maxSoFar)
+        return maxSoFar
+       
